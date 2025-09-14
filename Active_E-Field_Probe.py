db0021f828d57d5056a2ac5d9af00469ae3829f3
Martijn Roos <martijn.g.e.roos@gmail.com>
2025-09-14 20:18:40 +0200

    nullor implementation v1

--- Active_E-Field_Probe.py
+++ Active_E-Field_Probe.py
@@ -7,7 +7,8 @@ fileName = "Active_E-Field_Probe"
 # KiCAD version 8.0
 fileName = 'KiCad/' + fileName + '/' + fileName + '.kicad_sch'
 
-cir = sl.makeCircuit(fileName,expansion=True,imgWidth=400)
+cir = sl.makeCircuit(fileName,imgWidth=400)
+
 
 # Let us define an instruction to display the symbolic MNA matrix equation.
 MNA = sl.doMatrix(cir)
@@ -29,3 +30,4 @@ sl.eqn2html('M', MNA.M, label = 'M', labelText = 'MNA matrix')
 # 3. The vercor wit dependent variables 'Dv':
 sl.text2html('The vector with dependent variables is:')
 sl.eqn2html('D_v', MNA.Dv, label = 'Dv', labelText = 'Vector with dependent variables')
+
