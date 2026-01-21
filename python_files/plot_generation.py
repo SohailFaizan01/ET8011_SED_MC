5ac4932ec67387795dfec0021dbfc2b049064012
Sohail Faizan <sohailf@iitbhilai.ac.in>
2026-01-21 13:48:33 +0100

    Fix Noise Plotting

--- python_files/plot_generation.py
+++ python_files/plot_generation.py
@@ -1,6 +1,7 @@
 ################################################# Generate Plots for HTML pages #################################################
 
 from SLiCAP import *
+from sympy import cancel, Number, expand
 from .circuit import cir
 from .optimize_W_IC import noise_expr
 
@@ -12,6 +13,28 @@ servo       = doLaplace(cir, numeric=True, source='V1', detector='V_Amp_out', pa
 direct      = doLaplace(cir, numeric=True, source='V1', detector='V_Amp_out', pardefs='circuit', lgref='Gm_M1_X1', transfer='direct')
 
 # --- Plots Data Generation ---
+noise_expr.inoise = cancel(noise_expr.inoise)
+
+# --- Scale noise expression to avoid numerical overflow ---
+try:
+    num, den = noise_expr.inoise.as_numer_denom()
+    # Find all number literals in both numerator and denominator
+    atoms = num.atoms(Number).union(den.atoms(Number))
+    if atoms:
+        # Get absolute values of all numeric coefficients
+        coeffs = [abs(c.evalf()) for c in atoms]
+        if coeffs:
+            max_coeff = max(coeffs)
+            # Heuristic to decide if scaling is needed
+            if max_coeff > 1e50:
+                # Scale numerator and denominator
+                num_scaled = expand(num / max_coeff)
+                den_scaled = expand(den / max_coeff)
+                noise_expr.inoise = num_scaled / den_scaled
+except Exception:
+    # If scaling fails, proceed with the unscaled expression.
+    pass
+# --- End of scaling ---
 fb_model = [gain, asymptotic, loopgain, servo, direct]
 plotSweep("fb_mag", "Magnitude plots feedback model parameters", fb_model, 1e3, 1e10, 200, yLim=[-75, 75], funcType='dBmag')
 plotSweep("inoise", "input noise spectral density", [noise_expr], 1e3, 1e9, 200, funcType='inoise')
